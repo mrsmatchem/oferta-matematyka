fac69bde296139746da22185b339570670960d1f
kysniw <kuba.winsyk.forgames@gmail.com>
2023-11-24 23:44:23 +0100

    Reworked scrolling to top button and minor change in background

--- script.js
+++ script.js
@@ -1,28 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   const video = document.getElementsByClassName("marketing__video")[0];
-  const testimonialsContainer = document.querySelector(".testimonials");
-  const testimonials = document.querySelectorAll(".testimonial");
+  const iconToTop = document.querySelector(".footer-icon-to-top");
+  const header = document.querySelector(".header");
 
-  console.log(testimonials);
-
-  const testimonialsInColumn = Math.ceil(testimonials.length / 2);
-
-  let maxHeightTestimonialParent = 0;
-
-  for (let i = 0; i < testimonialsInColumn; i++) {
-    maxHeightTestimonialParent += testimonials[i].offsetHeight;
-  }
-
-  // testimonialsContainer.style = `height: ${maxHeightTestimonialParent + 200}px`;
-
-  console.log(maxHeightTestimonialParent);
-
-  console.log(testimonialsInColumn);
+  // console.log(header);
 
   window.addEventListener("scroll", () => {
     const scrollTopLine = window.scrollY;
     const scrollBottomLine = window.scrollY + window.innerHeight;
     const videoCenterLine = video.offsetTop + video.offsetHeight / 2;
+
+    if (scrollTopLine > header.clientHeight) {
+      iconToTop.classList.remove("invisible");
+    } else {
+      iconToTop.classList.add("invisible");
+    }
 
     if (scrollBottomLine > videoCenterLine && scrollTopLine < videoCenterLine) {
       video.play();
